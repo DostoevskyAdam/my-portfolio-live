29cb93fb8adf841829995e036c9fad9cb2175b00
Adam Pitcher <g94asp@mun.ca>
2021-10-26 23:43:17 -0230

    Updates

---
+++ precache-manifest.dd3ed032086bb8dd5259679ebc5d1396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac14a5130565d10eeb999eaca2deb67",
+    "url": "/DostoevskyAdam/my-portfolio-live/index.html"
+  },
+  {
+    "revision": "4e683d0b26944adfc1a6",
+    "url": "/DostoevskyAdam/my-portfolio-live/static/css/main.ea58596f.chunk.css"
+  },
+  {
+    "revision": "6f9d03cb831364098300",
+    "url": "/DostoevskyAdam/my-portfolio-live/static/js/2.507db340.chunk.js"
+  },
+  {
+    "revision": "4e683d0b26944adfc1a6",
+    "url": "/DostoevskyAdam/my-portfolio-live/static/js/main.ea4f4157.chunk.js"
+  },
+  {
+    "revision": "1d5e5166bd380df5dcaf",
+    "url": "/DostoevskyAdam/my-portfolio-live/static/js/runtime-main.730a83d1.js"
+  }
+]);
